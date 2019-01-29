774fe7ead5c34f0ca3cde7543908a9a6fc7d8aae
wandsen <dsenwan@gmail.com>
2019-01-30 02:20:39 +0800

    add bodyparser

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 const path = require('path');
 
 const port =  process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //Static file declaration
 app.use(express.static(path.join(__dirname, 'client/build')));
