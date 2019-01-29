97af552386cd730f7d45b731b47edaed31b1203f
wandsen <dsenwan@gmail.com>
2019-01-30 04:04:53 +0800

    changed position of routes to not intefere with wildcard

--- server.js
+++ server.js
@@ -12,6 +12,20 @@ const port =  process.env.PORT || 5000;
 //Static file declaration
 app.use(express.static(path.join(__dirname, 'client/build')));
 
+
+//routes
+app.get('/api/customers', (req, res) => {
+  const customers  = [
+    {id: 1, firstName: 'John', lastName: 'Doe'},
+    {id: 2, firstName: 'Steve', lastName: 'Smith'},
+    {id: 3, firstName: 'Mary', lastName: 'Swanson'}
+  ];
+  
+  res.json(customers);
+
+})
+
+
 //production mode
 if(process.env.NODE_ENV === 'production') {
   console.log('server in production mode')
@@ -27,15 +41,6 @@ if(process.env.NODE_ENV === 'production') {
 //   res.sendFile(path.join(__dirname+'/client/public/index.html'));
 // })
 
-app.get('/api/customers', (req, res) => {
-  const customers  = [
-    {id: 1, firstName: 'John', lastName: 'Doe'},
-    {id: 2, firstName: 'Steve', lastName: 'Smith'},
-    {id: 3, firstName: 'Mary', lastName: 'Swanson'}
-  ];
-  
-  res.json(customers);
 
-})
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
